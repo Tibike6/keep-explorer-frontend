904de27b59405db95264a38bd5f850dc7199069d
Tibike <Tibike@gmx.at>
2020-08-01 11:15:55 +0200

    Add heroku postbuild

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/keep-explorer'));
+app.use(express.static(__dirname + '/dist/keep-explorer'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/keep-explorer/'}),
-);
+  res.sendFile(path.join(__dirname+'/dist/keep-explorer/index.html')
+));
 
 app.listen(process.env.PORT || 8080);
