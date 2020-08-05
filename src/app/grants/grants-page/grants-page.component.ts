da29eb348a61436b67f591754b86f6e782fa7913
Tibike <Tibike@gmx.at>
2020-08-05 14:22:43 +0200

    Remove analytics page, add grants page

---
+++ src/app/grants/grants-page/grants-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-grants-page',
+    templateUrl: './grants-page.component.html',
+    styleUrls: ['./grants-page.component.scss']
+})
+export class GrantsPageComponent implements OnInit {
+    constructor() {}
+    ngOnInit(): void {}
+}
