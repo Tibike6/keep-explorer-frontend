e110317bdf0df3c80f83c7bdeb2b8c6fba7a961e
Tibike <Tibike@gmx.at>
2020-08-12 22:54:28 +0200

    Fix grant doughnut chart center text bug

--- src/app/grant/grant-list/grant-list.component.ts
+++ src/app/grant/grant-list/grant-list.component.ts
@@ -225,41 +225,43 @@ export class GrantListComponent implements OnInit, OnDestroy {
             ]
         ];
 
-        this.doughnutChartPlugins.push({
-            beforeDraw(chart: any) {
-                const ctx = chart.ctx;
-                // const txt = 'Total Issued: ' + this.grantStats?.totalIssued;
-                const txt = `Issued: ${grantStats.totalIssued.toLocaleString('en-US')}`;
-                const txt2 = `Grant Count: ${grantStats.totalCount}`;
+        if (this.doughnutChartPlugins.length === 0) {
+            this.doughnutChartPlugins.push({
+                beforeDraw(chart: any) {
+                    const ctx = chart.ctx;
+                    // const txt = 'Total Issued: ' + this.grantStats?.totalIssued;
+                    const txt = `Issued: ${grantStats.totalIssued.toLocaleString('en-US')}`;
+                    const txt2 = `Grant Count: ${grantStats.totalCount}`;
 
-                // Get options from the center object in options
-                const sidePadding = 65;
-                const sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2);
+                    // Get options from the center object in options
+                    const sidePadding = 65;
+                    const sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2);
 
-                ctx.textAlign = 'center';
-                ctx.textBaseline = 'middle';
-                const centerX = (chart.chartArea.left + chart.chartArea.right) / 2;
-                const centerY = (chart.chartArea.top + chart.chartArea.bottom) / 2;
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'middle';
+                    const centerX = (chart.chartArea.left + chart.chartArea.right) / 2;
+                    const centerY = (chart.chartArea.top + chart.chartArea.bottom) / 2;
 
-                // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
-                const stringWidth = ctx.measureText(txt).width;
-                const elementWidth = chart.innerRadius * 2 - sidePaddingCalculated;
+                    // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+                    const stringWidth = ctx.measureText(txt).width;
+                    const elementWidth = chart.innerRadius * 2 - sidePaddingCalculated;
 
-                // Find out how much the font can grow in width.
-                const widthRatio = elementWidth / stringWidth;
-                const newFontSize = Math.floor(30 * widthRatio);
-                const elementHeight = chart.innerRadius * 2;
+                    // Find out how much the font can grow in width.
+                    const widthRatio = elementWidth / stringWidth;
+                    const newFontSize = Math.floor(30 * widthRatio);
+                    const elementHeight = chart.innerRadius * 2;
 
-                // Pick a new font size so it will not be larger than the height of label.
-                const fontSizeToUse = Math.min(newFontSize, elementHeight);
+                    // Pick a new font size so it will not be larger than the height of label.
+                    const fontSizeToUse = Math.min(newFontSize, elementHeight);
 
-                ctx.font = fontSizeToUse + 'px Arial';
-                ctx.fillStyle = 'black';
+                    ctx.font = fontSizeToUse + 'px Arial';
+                    ctx.fillStyle = 'black';
 
-                // Draw text in center
-                ctx.fillText(txt2, centerX, centerY - fontSizeToUse / 2 - 3);
-                ctx.fillText(txt, centerX, centerY + fontSizeToUse / 2);
-            }
-        });
+                    // Draw text in center
+                    ctx.fillText(txt2, centerX, centerY - fontSizeToUse / 2 - 3);
+                    ctx.fillText(txt, centerX, centerY + fontSizeToUse / 2);
+                }
+            });
+        }
     }
 }
